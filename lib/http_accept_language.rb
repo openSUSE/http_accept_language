e735b5036608cae547aa9811b188a79365bec0df
Sebastian Cohnen <sebastian.cohnen@gmx.net>
2010-11-22 18:36:19 +0100

    HttpAcceptLanguage needs to be included to ActionDispatch::Request
    To work properly with Rails 3, HttpAcceptLanguage
    to be included to ActionDispatch::Request.

--- lib/http_accept_language.rb
+++ lib/http_accept_language.rb
@@ -62,4 +62,6 @@ elsif defined?(ActionDispatch::AbstractRequest)
   ActionDispatch::AbstractRequest.send :include, HttpAcceptLanguage
 elsif defined?(ActionDispatch::CgiRequest)
   ActionDispatch::CgiRequest.send :include, HttpAcceptLanguage
+elsif defined?(ActionDispatch::Request)
+  ActionDispatch::Request.send :include, HttpAcceptLanguage
 end
